c931dc71196a08144047e38faa345ec578be1c2d
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-05-06 12:03:22 -0300

    improved examples, added cloned event

--- example/example.js
+++ example/example.js
@@ -1,18 +1,22 @@
 'use strict';
 
-dragula([left1, right1]);
-dragula([left2, right2], { copy: true });
-dragula([left3, right3]).on('drag', function (el) {
+function $ (id) {
+  return document.getElementById(id);
+}
+
+dragula([$('left1'), $('right1')]);
+dragula([$('left2'), $('right2')], { copy: true });
+dragula([$('left3'), $('right3')]).on('drag', function (el) {
   el.className = el.className.replace(' ex-moved', '');
 }).on('drop', function (el) {
   setTimeout(function () {
     el.className += ' ex-moved';
   }, 0);
 });
-dragula([left4, right4], { revertOnSpill: true });
-dragula([left5, right5], {
+dragula([$('left4'), $('right4')], { revertOnSpill: true });
+dragula([$('left5'), $('right5')], {
   moves: function (el, container, handle) {
     return handle.className === 'handle';
   }
 });
-dragula([single1], { removeOnSpill: true });
+dragula([$('single1')], { removeOnSpill: true });
