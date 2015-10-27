d72c3edba80fbd3bf8f5f87460b6b06d2c5d7b76
dduvall <don@deduvall.com>
2015-10-27 10:57:12 -0700

    removed extra space in example

--- example/example.js
+++ example/example.js
@@ -17,7 +17,6 @@ dragula([$('left-events'), $('right-events')])
   })
   .on('out', function (el, container) {
     container.className = container.className.replace('ex-over', '');
-    
   });
 dragula([$('left-rollbacks'), $('right-rollbacks')], { revertOnSpill: true });
 dragula([$('left-lovehandles'), $('right-lovehandles')], {
