9e8159d1ba0702a76286ddc448c49ac94f063e05
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-02-05 16:21:02 -0300

    click fixes for #261

--- dragula.js
+++ dragula.js
@@ -513,8 +513,8 @@ function touchy (el, op, type, fn) {
 
 function whichMouseButton (e) {
   if (e.touches !== void 0) { return e.touches.length; }
+  if (e.which !== void 0 && e.which !== 0) { return e.which; } // see https://github.com/bevacqua/dragula/issues/261
   if (e.buttons !== void 0) { return e.buttons; }
-  if (e.which !== void 0) { return e.which; }
   var button = e.button;
   if (button !== void 0) { // see https://github.com/jquery/jquery/blob/99e8ff1baa7ae341e94bb89c3e84570c7c3ad9ea/src/event.js#L573-L575
     return button & 1 ? 1 : button & 2 ? 3 : (button & 4 ? 2 : 0);
