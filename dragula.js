81f13d46f5ad8c5c27ae0076b1c4ad5c3b48efaf
a.poterenko <a.poterenko@crif.com>
2017-06-15 18:36:15 +0300

    https://github.com/bevacqua/dragula/issues/501

--- dragula.js
+++ dragula.js
@@ -123,13 +123,9 @@ function dragula (initialContainers, options) {
       release({});
       return; // when text is selected on an input and then dragged, mouseup doesn't fire. this is our only hope
     }
-    // truthy check fixes #239, equality fixes #207
-    if (e.clientX !== void 0 && e.clientX === _moveX && e.clientY !== void 0 && e.clientY === _moveY) {
-      return;
-    }
-    // #501
-    if ((Math.abs(e.clientX - _moveX) <= o.slideFactorX || o.slideFactorX === void 0)
-      && (Math.abs(e.clientY - _moveY) <= o.slideFactorY || o.slideFactorY === void 0)) {
+    // truthy check fixes #239, equality fixes #207 & #501
+    if ((e.clientX !== void 0 && Math.abs(e.clientX - _moveX) <= (o.slideFactorX || 0))
+      && (e.clientY !== void 0 && Math.abs(e.clientY - _moveY) <= (o.slideFactorY || 0))) {
       return;
     }
     if (o.ignoreInputTextSelection) {
