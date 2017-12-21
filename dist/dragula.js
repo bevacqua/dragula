bb729b9c024038ea7b8449f5a70e1e7c4a5c1980
janus j k lu <januslo6211@icloud.com>
2017-12-21 17:08:48 +0800

    Reserves witch button detect logic
    Reserves witch button detect logic

--- dist/dragula.js
+++ dist/dragula.js
@@ -556,13 +556,15 @@ function touchy (el, op, type, fn) {
 }
 
 function whichMouseButton (e) {
-  if (e.touches !== void 0) { return e.touches.length; }
-  if (e.which !== void 0 && e.which !== 0) { return e.which; } // see https://github.com/bevacqua/dragula/issues/261
-  if (e.buttons !== void 0) { return e.buttons; }
-  var button = e.button;
-  if (button !== void 0) { // see https://github.com/jquery/jquery/blob/99e8ff1baa7ae341e94bb89c3e84570c7c3ad9ea/src/event.js#L573-L575
-    return button & 1 ? 1 : button & 2 ? 3 : (button & 4 ? 2 : 0);
-  }
+   if (e.touches) { return e.touches.length; }
+	    if (e.originalEvent && e.originalEvent.touches) { return e.originalEvent.touches.length; }
+	    if (e.which !== void 0 && e.which !== 0) { return e.which; } // github.com/bevacqua/dragula/issues/261
+	    var button = e.button;
+	    if (button !== undefined) { // see github.com/jquery/jquery/blob/99e8ff1baa7ae341e94bb89c3e84570c7c3ad9ea/src/event.js#L573-L575
+	      // button == 0?1:** for the e object was unwrapped orginalEvent object in jQuery enviroment.
+	      return button==0?1:button & 1 ? 1 : button & 2 ? 3 : (button & 4 ? 2 : 0);
+	    }
+	    if (e.buttons !== undefined) { return e.buttons; }
 }
 
 function getOffset (el) {
